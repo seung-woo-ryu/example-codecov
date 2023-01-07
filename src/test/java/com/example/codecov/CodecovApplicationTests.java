b1bc8a4d6767f918b635f95d1a8593dbaa818b32
seung-woo-ryu <37530565+seung-woo-ryu@users.noreply.github.com>
2023-01-07 19:24:51 +0900

    Revert "add"

--- src/test/java/com/example/codecov/CodecovApplicationTests.java
+++ src/test/java/com/example/codecov/CodecovApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.codecov;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,9 +8,6 @@ class CodecovApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("test!");
-		System.out.println("test!");
-		Assertions.assertThat(1L).isEqualTo(1L);
 	}
 
 }
